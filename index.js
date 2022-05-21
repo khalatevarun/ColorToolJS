50f730b0f8b58dafdd237505bff3c327b0263947
Varun Hanuman Khalate <varunkhalate@BGVMC474.local>
2022-05-21 15:19:38 +0530

    convert RBG to HEX

--- index.js
+++ index.js
@@ -37,10 +37,24 @@ const convertHextoRGB = (hex) => {
         + strippedHex[2] + strippedHex[2]
     }
 
-    const r = parseInt(strippedHex.substring(0,2),16);
+    const r = parseInt(strippedHex.substring(0,2),16);  // to convert hex value to a decimal value ranging from 0 - 255
     const g = parseInt(strippedHex.substring(2,4),16);
     const b = parseInt(strippedHex.substring(4,6),16);
 
     return {r,g,b}
+}
+
+const convertRBGToHex = (r,g,b) => {
+    
+    // have passed 16 in toString() since we need to convert it to hex value
+    // adding 0 and then slicing the whole string since for 0 the hex value generated is single character '0', but we need two so we concate and then use slice to get last two characters
+    // above operation also handles cases which generate two characters, adding 0 and then slicing the string will give us 2 characters everytime.
+
+    const firstPair = ("0" + r.toString(16)).slice(-2);  
+    const secondPair = ("0" + g.toString(16)).slice(-2);
+    const thirdPair = ("0" + b.toString(16)).slice(-2);
+
+    const hex = "#" + firstPair + secondPair + thirdPair;
+    return hex;
 }
 
