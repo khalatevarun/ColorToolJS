c9254bf2d27754601a95c04cf19882697007babb
Varun Hanuman Khalate <varunkhalate@BGVMC474.local>
2022-05-21 14:36:14 +0530

    convert HEX color to RGB

--- index.js
+++ index.js
@@ -24,3 +24,23 @@ const isValidHex = (hex) => {
 
     return regex.test(strippedHex);  
 }
+
+
+const convertHextoRGB = (hex) => {
+    if(!isValidHex(hex)) return null;
+
+    let strippedHex = hex.replace('#','');
+
+    if(strippedHex.length === 3){
+        strippedHex = strippedHex[0] + strippedHex[0]
+        + strippedHex[1] + strippedHex[1]
+        + strippedHex[2] + strippedHex[2]
+    }
+
+    const r = parseInt(strippedHex.substring(0,2),16);
+    const g = parseInt(strippedHex.substring(2,4),16);
+    const b = parseInt(strippedHex.substring(4,6),16);
+
+    return {r,g,b}
+}
+
